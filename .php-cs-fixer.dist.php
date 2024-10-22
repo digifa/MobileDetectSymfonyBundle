8da066d4693331faaf202f2031a44bae8e73c527
Faust <faust@cpa.de>
2024-10-22 09:40:16 +0200

    Project Setup
    - update .gitignore
    - update README.md and Documentation
    - add bundles.php
    - add .editorconfig
    - add .php-cs-fixer.dist.php
    - add phpcs.xml.dist
    - add phpstan.neon.dist
    - add rector.php
    - add renovate.json
    - add codecov.yml
    - add Makefile
    - add Github CI Workflow
    - add tests/

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@DoctrineAnnotation' => true,
+        '@PhpCsFixer:risky' => true,
+        '@PhpCsFixer' => true,
+        '@PHPUnit84Migration:risky' => true,
+        '@PSR1' => true,
+        '@PSR12:risky' => true,
+        '@PSR12' => true,
+        '@PSR2' => true,
+        '@Symfony:risky' => true,
+        '@Symfony' => true,
+        'array_indentation' => true,
+        'array_syntax' => [
+            'syntax' => 'short',
+        ],
+        'doctrine_annotation_array_assignment' => [
+            'operator' => '=',
+        ],
+        'doctrine_annotation_spaces' => [
+            'after_array_assignments_equals' => false,
+            'before_array_assignments_equals' => false,
+        ],
+        'linebreak_after_opening_tag' => true,
+        'list_syntax' => [
+            'syntax' => 'short',
+        ],
+        'no_extra_blank_lines' => [
+            'tokens' => [
+                'break',
+                'continue',
+                'curly_brace_block',
+                'extra',
+                'parenthesis_brace_block',
+                'return',
+                'square_brace_block',
+                'throw',
+                'use',
+            ],
+        ],
+        'no_superfluous_phpdoc_tags' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'ordered_class_elements' => true,
+        'ordered_imports' => [
+            'imports_order' => [
+                'class',
+                'function',
+                'const',
+            ],
+            'sort_algorithm' => 'alpha',
+        ],
+        'php_unit_method_casing' => [
+            'case' => 'camel_case',
+        ],
+        'phpdoc_order' => true,
+        'phpdoc_trim_consecutive_blank_line_separation' => true,
+        'single_line_comment_style' => true,
+        'strict_comparison' => true,
+        'strict_param' => true,
+        'void_return' => false,
+    ])
+    ->setFinder($finder)
+    ;
